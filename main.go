ebe2b9b469de2b8b6ba8dc4900a95df9ceef5287
Daniel Sont <dan.sont@gmail.com>
2015-11-29 12:22:32 -0500

    processor

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/gorilla/websocket"
 	"encoding/json"
+	"strings"
 )
 
 const (
@@ -13,9 +14,10 @@ const (
   messageBufferSize = 256
 )
 
+var name = "ds0nt-bot"
+
 type client struct {
   socket *websocket.Conn
-  name   string
   send   chan message
 }
 
@@ -24,8 +26,45 @@ type message struct {
   Payload interface{} `json:"payload"`
 }
 
+var responder *processor
+
+type processor struct {
+	In  chan *message
+	Out chan *message
+}
+
+func newProcessor(client *client) *processor {
+	in := make(chan *message, messageBufferSize)
+	out := make(chan *message, messageBufferSize)
+	return &processor{in, out}
+}
+
+func (p *processor) process() {
+	p.Out <-&message{"nick", name}
+	log.Println("Starting processor")
+	for msg := range p.In {
+		if msg.Type == "chat" {
+			text := msg.Payload.(string)
+			if strings.HasPrefix(text, name) {
+				continue
+			}
+			log.Println("I hear stuff", text)
+			if strings.Contains(text, name) {
+				log.Println("Someone is talking to me:", text)
+				p.Out <-&message{"chat", "Du hast mich gefragt, und ich hab Nichsts gesact..."}
+			}
+		}
+	}
+}
+
+func (p *processor) resultsTo(client *client) {
+	for msg := range p.Out {
+    client.send <- *msg
+	}
+}
+
 func newClient(socket *websocket.Conn, name string) *client {
-  return &client{socket, name, make(chan message, messageBufferSize)}
+  return &client{socket, make(chan message, messageBufferSize)}
 }
 
 func (c *client) read() {
@@ -39,6 +78,7 @@ func (c *client) read() {
         continue
       }
       log.Printf("Message Received: %v", f)
+			responder.In <-&f
     } else {
       break
     }
@@ -70,6 +110,9 @@ func dial(url string) {
 	}
 
   client := newClient(ws, "ds0nt-bot")
+	responder = newProcessor(client)
+	go responder.process()
+	go responder.resultsTo(client)
   go client.write()
   client.read()
 }
